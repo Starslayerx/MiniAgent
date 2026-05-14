5c5f623d792640b5a8d04e63c9cb456c8ebf706c
Starslayerx <1049353754@qq.com>
2026-05-14 14:16:22 +0800

    feat: add compress prompts

---
+++ prompts/compress.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiofiles
+from pathlib import Path
+
+
+async def read_single_file(filepath: Path):
+    async with aiofiles.open(filepath, 'r', encoding='utf-8') as f:
+        return await f.read()
+
+async def get_compress_prompts() -> tuple[str, str]:
+    folder = Path(__file__).parent / 'templates' / 'compress'
+    files = [folder / 'summary.md', folder / 'summary_prefix.md']
+    tasks = [read_single_file(file) for file in files]
+    contents = await asyncio.gather(*tasks)
+    results = tuple(contents)
+    return results
