8ce4ba0e6a5d5cef8e5a83ded9bf0165818aef66
Starslayerx <1049353754@qq.com>
2026-05-15 10:44:24 +0800

    fix: typo

--- tools/registry.py
+++ tools/registry.py
@@ -2,14 +2,14 @@ from agent.context import AgentContext
 from .system import tools as system_tools, tool_handlers as system_tool_handlers
 from .plan import build_plan_registry
 from .subagent import build_subagent_registry
-from .skill import bulid_skill_registry, SkillRegistry
+from .skill import build_skill_registry, SkillRegistry
 
 
 async def build_child_registry(skill_registry: SkillRegistry):
     """Return child tools"""
 
     plan_tools, plan_tool_handlers = await build_plan_registry()
-    skill_tools, skill_tool_handlers = await bulid_skill_registry(registry=skill_registry)
+    skill_tools, skill_tool_handlers = await build_skill_registry(registry=skill_registry)
 
     child_tools = [*plan_tools, *system_tools, *skill_tools]
     child_tool_handlers = plan_tool_handlers | system_tool_handlers | skill_tool_handlers
